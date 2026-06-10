7e13d386858d0a98ac0d50dd2b8365e8a9179fac
Scalabrini17 <itallomartins2007@gmail.com>
2026-06-10 16:14:52 -0300

    update: Iniciado a configuração da view do admin (ainda terá mais update da view)

--- apps/GTP/admin.py
+++ apps/GTP/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'prioridade' ,'prazo', 'status')
+
+    
 # Register your models here.
