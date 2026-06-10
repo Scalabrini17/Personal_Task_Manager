4f36d9dec6ab3ead0dedea57899ca25cd0a8f4ab
Scalabrini17 <itallomartins2007@gmail.com>
2026-06-10 14:35:56 -0300

    first commit

---
+++ apps/GTP/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GtpConfig(AppConfig):
+    name = 'apps.GTP'
---
+++ apps/GTP/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Prioridade(models.TextChoices):
+      URGENTE =   'urgente', 'Urgente'
+      ALTA = 'alta', 'Alta'
+      MEDIA = 'media', 'Média'
+      BAIXA = 'baixa', 'Baixa'
+
+class Status(models.TextChoices):
+      EM_ESPERA =  'em_espera', 'Em Espera'
+      INICIADO = 'iniciado', 'Iniciado'
+      FINALIZADO = 'finalizado', 'Finalizado'
+
+
+class Task(models.Model):
+      nome = models.CharField(max_length = 50)
+      descricao = models.TextField()
+      prazo = models.DateField()
+      prioridade = models.CharField(max_length = 20, choices = Prioridade.choices, default = Prioridade.MEDIA)
+      status = models.CharField(max_length = 20, choices = Status.choices, default = Status.EM_ESPERA)
+    
+      def __str__(self):
+            return self.nome
+      
