65ff3d32256709ee25e199128583bb8fd0085342
Scalabrini17 <itallomartins2007@gmail.com>
2026-06-16 13:18:48 -0300

    Update: Adicionado status e prioridade ao tamplate

--- apps/GTP/forms.py
+++ apps/GTP/forms.py
@@ -19,6 +19,6 @@ class TaskForms(forms.ModelForm):
             'nome': forms.TextInput(attrs={'class':'form-control'}),
             'descricao': forms.TextInput(attrs={'class':'form-control'}),
             'prazo': forms.DateInput(format= '%d/%m/%Y', attrs={'type': 'date', 'class':'form-control'}),
-            'prioridade': forms.Select(attrs={'class':'form-control'}),
-            'status': forms.Select(attrs={'class':'form-control'}),
+            'prioridade': forms.Select(attrs={'class':'form-select'}),
+            'status': forms.Select(attrs={'class':'form-select'}),
         }
